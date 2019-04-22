350f69b06ea2c023bd75d9269f24c33c07118d5d
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2019-04-22 16:23:38 +0900

    add terminator config

--- ln_setup.sh
+++ ln_setup.sh
@@ -6,21 +6,6 @@ EMACSDIR=~/dev/src/github.com/shishi/emacs
 ln -s $DOTDIR/fish ~/.config/fish
 ln -s $EMACSDIR ~/.emacs.d
 
-if [ `uname` = Darwin ]; then
-    ln -s $DOTDIR/.gitconfig.mac ~/.gitconfig
-    ln -s $DOTDIR/Brewfile ~/Brewfile
-elif [ `uname` = Linux ]; then
-    ln -s $DOTDIR/.gitconfig.linux ~/.gitconfig
-#    ln -s $DOTDIR/.xprofile ~/.xprofile
-#    ln -s $DOTDIR/.xbindkeysrc ~/.xbindkeysrc
-#    ln -s $DOTDIR/.imwheelrc ~/.imwheelrc
-#    ln -s $DOTDIR/imwheel.desktop ~/.config/autostart/imwheel.desktop
-#    ln -s $DOTDIR/fonts.conf ~/.config/fontconfig/fonts.conf
-#    fc-cache -fv
-elif [ `uname` = MINGW64_NT-10.0 ]; then
-    ln -s $DOTDIR/.gitconfig.win ~/.gitconfig
-fi
-
 ln -s $DOTDIR/.gitignore ~/.gitignore
 
 ln -s $DOTDIR/.vimrc ~/.vimrc
@@ -34,5 +19,21 @@ ln -s $DOTDIR/.pryrc ~/.pryrc
 # ln -s $DOTDIR/.zsh ~/.zsh
 # ln -s $DOTDIR/.zshenv ~/.zshenv
 # ln -s $DOTDIR/.zshrc ~/.zshrc
+
+if [ `uname` = Darwin ]; then
+    ln -s $DOTDIR/.gitconfig.mac ~/.gitconfig
+    ln -s $DOTDIR/Brewfile ~/Brewfile
+elif [ `uname` = Linux ]; then
+    ln -s $DOTDIR/terminator ~/.config/terminator
+    ln -s $DOTDIR/.gitconfig.linux ~/.gitconfig
+#    ln -s $DOTDIR/.xprofile ~/.xprofile
+#    ln -s $DOTDIR/.xbindkeysrc ~/.xbindkeysrc
+#    ln -s $DOTDIR/.imwheelrc ~/.imwheelrc
+#    ln -s $DOTDIR/imwheel.desktop ~/.config/autostart/imwheel.desktop
+#    ln -s $DOTDIR/fonts.conf ~/.config/fontconfig/fonts.conf
+#    fc-cache -fv
+elif [ `uname` = MINGW64_NT-10.0 ]; then
+    ln -s $DOTDIR/.gitconfig.win ~/.gitconfig
+fi
 
 echo "please reload shell"
