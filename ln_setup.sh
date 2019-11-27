0f91ea28fca5adba9e4523de18fe221ef3092a34
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2019-11-27 21:37:54 +0900

    improve ln_setup

--- ln_setup.sh
+++ ln_setup.sh
@@ -1,56 +1,56 @@
 #!/bin/sh
 
-DOTDIR=$(dirname "$0")
-EMACSDIR=~/dev/src/github.com/shishi/emacs
+DOTDIR=$(pwd)
+#$EMACSDIR=~/dev/src/github.com/shishi/emacs
 
 if [ -L ~/.config/fish ]; then
     rm ~/.config/fish
-    ln -s $DOTDIR/fish ~/.config/fish
+    ln -sf $DOTDIR/fish ~/.config/fish
 elif [ -d ~/.config/fish ]; then
     rm -fr ~/.config/fish
-    ln -s $DOTDIR/fish ~/.config/fish
+    ln -sf $DOTDIR/fish ~/.config/fish
 else
-    ln -s $DOTDIR/fish ~/.config/fish
+    ln -sf $DOTDIR/fish ~/.config/fish
 fi
 
 if [ -L ~/.emacs.d ]; then
     rm ~/.emacs.d
-    ln -s $EMACSDIR ~/.emacs.d
+    ln -sf $DOTDIR/../emacs ~/.emacs.d
 elif [ -d ~/.emacs.d ]; then
     rm -fr ~/.emacs.d
-    ln -s $EMACSDIR ~/.emacs.d
+    ln -sf $DOTDIR/../emacs ~/.emacs.d
 else
-    ln -s $EMACSDIR ~/.emacs.d
+    ln -sf $DOTDIR/../emacs ~/.emacs.d
 fi
 
-ln -s $DOTDIR/.gitignore ~/.gitignore
+ln -sf $DOTDIR/.gitignore ~/.gitignore
 
-ln -s $DOTDIR/.vimrc ~/.vimrc
-ln -s $DOTDIR/.gvimrc ~/.gvimrc
-ln -s $DOTDIR/.vim ~/.vim
+ln -sf $DOTDIR/.vimrc ~/.vimrc
+ln -sf $DOTDIR/.gvimrc ~/.gvimrc
+ln -sf $DOTDIR/.vim ~/.vim
 
-ln -s $DOTDIR/.gemrc ~/.gemrc
-ln -s $DOTDIR/.rspec ~/.rspec
-ln -s $DOTDIR/.pryrc ~/.pryrc
+ln -sf $DOTDIR/.gemrc ~/.gemrc
+ln -sf $DOTDIR/.rspec ~/.rspec
+ln -sf $DOTDIR/.pryrc ~/.pryrc
 
 # ln -s $DOTDIR/.zsh ~/.zsh
 # ln -s $DOTDIR/.zshenv ~/.zshenv
 # ln -s $DOTDIR/.zshrc ~/.zshrc
 
 if [ `uname` = Darwin ]; then
-    ln -s $DOTDIR/.gitconfig.mac ~/.gitconfig
-    ln -s $DOTDIR/Brewfile ~/Brewfile
+    ln -sf $DOTDIR/.gitconfig.mac ~/.gitconfig
+    ln -sf $DOTDIR/Brewfile ~/Brewfile
 elif [ `uname` = Linux ]; then
-    ln -s $DOTDIR/.gitconfig.linux ~/.gitconfig
-#    ln -s $DOTDIR/terminator ~/.config/terminator
-#    ln -s $DOTDIR/.xprofile ~/.xprofile
-#    ln -s $DOTDIR/.xbindkeysrc ~/.xbindkeysrc
-#    ln -s $DOTDIR/.imwheelrc ~/.imwheelrc
-#    ln -s $DOTDIR/imwheel.desktop ~/.config/autostart/imwheel.desktop
-#    ln -s $DOTDIR/fonts.conf ~/.config/fontconfig/fonts.conf
+    ln -sf $DOTDIR/.gitconfig.linux ~/.gitconfig
+#    ln -sf $DOTDIR/terminator ~/.config/terminator
+#    ln -sf $DOTDIR/.xprofile ~/.xprofile
+#    ln -sf $DOTDIR/.xbindkeysrc ~/.xbindkeysrc
+#    ln -sf $DOTDIR/.imwheelrc ~/.imwheelrc
+#    ln -sf $DOTDIR/imwheel.desktop ~/.config/autostart/imwheel.desktop
+#    ln -sf $DOTDIR/fonts.conf ~/.config/fontconfig/fonts.conf
 #    fc-cache -fv
 elif [ `uname` = MINGW64_NT-10.0 ]; then
-    ln -s $DOTDIR/.gitconfig.win ~/.gitconfig
+    ln -sf $DOTDIR/.gitconfig.win ~/.gitconfig
 fi
 
 echo "please reload shell"
