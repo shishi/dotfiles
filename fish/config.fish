71eaa1601cf5bf24a3ca10d38b925a847e16e871
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2019-11-29 18:14:13 +0900

    fish and ghq

--- fish/config.fish
+++ fish/config.fish
@@ -153,6 +153,19 @@ function su
     /bin/su --shell=/usr/bin/fish $argv
 end
 
+function __ghq_cd_repository -d "Change local repository directory"
+    ghq list --full-path | fzf | read -l repo_path
+      cd $repo_path
+end
+
+function __ghq_browse_github -d "Browse remote repository on github"
+    ghq list | fzf | read -l repo_path
+    set -l repo_name (string split -m1 "/" $repo_path)[2]
+    hub browse $repo_name
+end
+
+alias ghb __ghq_browse_github
+
 function remove_orphan
     if type yay > /dev/null 2>&1
         yay -Yc
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,9 @@
 function fish_user_key_bindings
     ### ghq ###
+    bind \cg '__ghq_cd_repository'
+    if bind -M insert >/dev/null ^/dev/null
+          bind -M insert \cg '__ghq_cd_repository'
+    end
     # bind \cs 'ghq_fzf'
     # bind \es 'ghq_fzf_godoc'
     ### ghq ###
