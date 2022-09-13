219265f376c1fd36ec3c43d234da12826b09a860
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-13 23:13:01 +0900

    n for neovim

--- fish/config.fish
+++ fish/config.fish
@@ -19,12 +19,12 @@ set -x GOPATH ~/dev/
 
 set -x GPG_TTY (tty)
 
-if [ "$TERM_PROGRAM" = "vscode" ]
-  set -x EDITOR code --wait
-  set -x VISUAL code --wait
+if [ "$TERM_PROGRAM" = vscode ]
+    set -x EDITOR code --wait
+    set -x VISUAL code --wait
 else
-  set -x EDITOR vim
-  set -x VISUAL vim
+    set -x EDITOR vim
+    set -x VISUAL vim
 end
 
 # if type emacsclient > /dev/null 2>&1
@@ -43,37 +43,37 @@ end
 #   set -x BROWSER "/home/shishi/dev/src/github.com/shishi/dotfiles/wsl_browser.sh"
 # end
 
-if [ (uname) = "Darwin" ]
-  if type gls &> /dev/null
-    set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-    set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-  end
+if [ (uname) = Darwin ]
+    if type gls &>/dev/null
+        set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+        set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+    end
 
-  if type gfind &> /dev/null
-    set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-    set -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
-  end
+    if type gfind &>/dev/null
+        set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+        set -x MANPATH /usr/local/opt/findutils/libexec/gnuman $MANPATH
+    end
 
-  if test -f ~/Applications/MacVim.app/Contents/MacOS/Vim
-    set -x PATH ~/Applications/MacVim.app/Contents/MacOS $PATH
-  end
+    if test -f ~/Applications/MacVim.app/Contents/MacOS/Vim
+        set -x PATH ~/Applications/MacVim.app/Contents/MacOS $PATH
+    end
 end
 
-if type less &> /dev/null
-  set -x LESS '-R'
+if type less &>/dev/null
+    set -x LESS -R
 end
 
 set -x LESS '-q --ignore-case --no-init --long-prompt --raw-control-chars'
 # set -x LESS '-q -N --ignore-case --no-init --long-prompt --raw-control-chars'
-if type lv &> /dev/null
-  set -x PAGER 'lv -c'
-# else
-#   set -x PAGER 'less -N --ignore-case -no-init --long-prompt --raw-control-chars'
+if type lv &>/dev/null
+    set -x PAGER 'lv -c'
+    # else
+    #   set -x PAGER 'less -N --ignore-case -no-init --long-prompt --raw-control-chars'
 end
 
 # rbenv
 if test -d ~/.rbenv/bin
-  set -x PATH ~/.rbenv/bin $PATH
+    set -x PATH ~/.rbenv/bin $PATH
 end
 
 # # ruby
@@ -83,18 +83,18 @@ end
 
 # cask
 if test -f ~/.cask/bin/cask
-  set -x PATH ~/.cask/bin $PATH
+    set -x PATH ~/.cask/bin $PATH
 end
 
 # rust
 if test -d ~/.cargo/bin
-  set -x PATH ~/.cargo/bin $PATH
-  set -x CARGO_NET_GIT_FETCH_WITH_CLI true
+    set -x PATH ~/.cargo/bin $PATH
+    set -x CARGO_NET_GIT_FETCH_WITH_CLI true
 end
 
-if type bat &> /dev/null
-  set -x BAT_THEME zenburn
-  set -x BAT_STYLE auto
+if type bat &>/dev/null
+    set -x BAT_THEME zenburn
+    set -x BAT_STYLE auto
 end
 # # vagrant in wsl
 # if type vagrant &> /dev/null
@@ -108,19 +108,19 @@ end
 # end
 
 # use buildkit
-if type docker &> /dev/null
-  set -x DOCKER_BUILDKIT 1
+if type docker &>/dev/null
+    set -x DOCKER_BUILDKIT 1
 end
 
 # flyio
-if test -d ~/.fly &> /dev/null
-  set -x FLYCTL_INSTALL ~/.fly
-  set -x PATH $FLYCTL_INSTALL/bin $PATH
+if test -d ~/.fly &>/dev/null
+    set -x FLYCTL_INSTALL ~/.fly
+    set -x PATH $FLYCTL_INSTALL/bin $PATH
 end
 
 # windows explorer
-if test /mnt/c/Windows/explorer.exe &> /dev/null
-  alias explorer /mnt/c/Windows/explorer.exe
+if test /mnt/c/Windows/explorer.exe &>/dev/null
+    alias explorer /mnt/c/Windows/explorer.exe
 end
 
 # settings
@@ -133,22 +133,23 @@ set default_user shishi
 
 # rbenv
 if test -d ~/.rbenv
-  status --is-interactive; and source (rbenv init -|psub)
+    status --is-interactive; and source (rbenv init -|psub)
 end
 
 # direnv
-if type direnv &> /dev/null
-  eval (direnv hook fish)
+if type direnv &>/dev/null
+    eval (direnv hook fish)
 end
 
 # alias
 #########################################
 
+alias n nvim
 alias grep 'grep --color'
 alias lv 'lv -c'
 
 # git
-alias g 'git'
+alias g git
 alias gs 'git status -sb'
 alias gl 'git log --graph --decorate --name-status'
 alias gg 'git grep'
@@ -166,53 +167,53 @@ alias rdmr 'bundle exec rake db:migrate:reset'
 alias rdmrs 'bundle exec rake db:migrate:reset; and bundle exec rake db:seed'
 
 # docker
-if type docker-compose &> /dev/null
-  alias dc 'docker-compose'
-  alias dcr 'docker-compose run --rm'
-  alias dce 'docker-compose exec'
+if type docker-compose &>/dev/null
+    alias dc docker-compose
+    alias dcr 'docker-compose run --rm'
+    alias dce 'docker-compose exec'
 else
-  alias dc 'docker compose'
-  alias dcr 'docker compose run --rm'
-  alias dce 'docker compose exec'
+    alias dc 'docker compose'
+    alias dcr 'docker compose run --rm'
+    alias dce 'docker compose exec'
 end
 
 
 switch (uname -a)
-  case "*MINGW64*"
-    alias ghq 'ghq.exe'
-    alias fzf 'fzf.exe'
-    alias docker 'docker.exe'
-    alias docker-compose 'docker-compose.exe'
-    alias docker-machine 'docker-machine.exe'
-  case "*Darwin*"
-    # alias brew_cask_upgrade 'for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed"; or brew cask install $c; done'
+    case "*MINGW64*"
+        alias ghq 'ghq.exe'
+        alias fzf 'fzf.exe'
+        alias docker 'docker.exe'
+        alias docker-compose 'docker-compose.exe'
+        alias docker-machine 'docker-machine.exe'
+    case "*Darwin*"
+        # alias brew_cask_upgrade 'for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed"; or brew cask install $c; done'
 
-    # ll
-    if type gls &> /dev/null
-      alias ll 'gls -la --color'
-    else
-      alias ll 'ls -laG'
-    end
+        # ll
+        if type gls &>/dev/null
+            alias ll 'gls -la --color'
+        else
+            alias ll 'ls -laG'
+        end
 
-    # macvim-kaoriya
-    if test -f ~/Applications/MacVim.app/Contents/MacOS/Vim
-      alias vi 'env LANG=ja_JP.UTF-8 ~/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-      alias vim 'env LANG=ja_JP.UTF-8 ~/Applications/MacVim.app/Contents/MacOpS/Vim "$@"'
-    end
-  case "*Linux*"
-      alias ll 'ls -la --color'
-    # alias open 'xdg-open'
+        # macvim-kaoriya
+        if test -f ~/Applications/MacVim.app/Contents/MacOS/Vim
+            alias vi 'env LANG=ja_JP.UTF-8 ~/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+            alias vim 'env LANG=ja_JP.UTF-8 ~/Applications/MacVim.app/Contents/MacOpS/Vim "$@"'
+        end
+    case "*Linux*"
+        alias ll 'ls -la --color'
+        # alias open 'xdg-open'
 end
 
 # WSL
 if test -f '/mnt/c/Users/shishi/AppData/Local/Programs/Microsoft VS Code/bin/code'
-  alias code '/mnt/c/Users/shishi/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
+    alias code '/mnt/c/Users/shishi/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
 end
 
 # rust tools
-if type exa &> /dev/null
-  alias ll 'exa -lah --git --icons --time-style=long-iso'
-  alias lt 'exa -T --icons --git-ignore'
+if type exa &>/dev/null
+    alias ll 'exa -lah --git --icons --time-style=long-iso'
+    alias lt 'exa -T --icons --git-ignore'
 end
 
 # if type bat &> /dev/null
@@ -226,66 +227,66 @@ end
 #########################################
 
 switch (uname -a)
-  case "*MINGW64*"
-  case "*Linux*" "Darwin*"
-    function su
-      /bin/su --shell=/usr/bin/fish $argv
-    end
+    case "*MINGW64*"
+    case "*Linux*" "Darwin*"
+        function su
+            /bin/su --shell=/usr/bin/fish $argv
+        end
 
-    function ibus_restart
-      ibus-daemon -drx
-    end
+        function ibus_restart
+            ibus-daemon -drx
+        end
 
-    # ghq
-    if type ghq &> /dev/null
-      function __ghq_cd_repository -d "Change local repository directory"
-        ghq list --full-path | fzf | read -l repo_path
-        cd $repo_path
-      end
-      alias ghc __ghq_cd_github
+        # ghq
+        if type ghq &>/dev/null
+            function __ghq_cd_repository -d "Change local repository directory"
+                ghq list --full-path | fzf | read -l repo_path
+                cd $repo_path
+            end
+            alias ghc __ghq_cd_github
 
-      function __ghq_browse_github -d "Browse remote repository on github"
-        ghq list | fzf | read -l repo_path
-        set -l repo_name (string split -m1 "/" $repo_path)[2]
-        hub browse $repo_name
-      end
-      alias ghb __ghq_browse_github
-    end
+            function __ghq_browse_github -d "Browse remote repository on github"
+                ghq list | fzf | read -l repo_path
+                set -l repo_name (string split -m1 "/" $repo_path)[2]
+                hub browse $repo_name
+            end
+            alias ghb __ghq_browse_github
+        end
 
-    # fzf
-    if type fzf &> /dev/null
-      function gb -d "Fuzzy-find and checkout a branch"
-        git branch --all | grep -v HEAD | string trim | fzf | xargs git checkout
-      end
-    end
+        # fzf
+        if type fzf &>/dev/null
+            function gb -d "Fuzzy-find and checkout a branch"
+                git branch --all | grep -v HEAD | string trim | fzf | xargs git checkout
+            end
+        end
 
-    function ln_setup
-      bash ~/dev/src/github.com/shishi/dotfiles/setup.sh
-    end
+        function ln_setup
+            bash ~/dev/src/github.com/shishi/dotfiles/setup.sh
+        end
 
-    function docker_run_with_current_user_and_dir
-      docker run -it --rm -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u (id -u $USER):(id -g $USER) -v (pwd):/src -w /src -e HOME=/src $argv
-    end
+        function docker_run_with_current_user_and_dir
+            docker run -it --rm -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u (id -u $USER):(id -g $USER) -v (pwd):/src -w /src -e HOME=/src $argv
+        end
 end
 
 # Arch
 
-if [ -f '/etc/arch-release' ]
-  function remove_orphan
-    if type yay &> /dev/null
-      yay -Yc
-    else
-      pacman -Rns (pacman -Qtdq)
+if [ -f /etc/arch-release ]
+    function remove_orphan
+        if type yay &>/dev/null
+            yay -Yc
+        else
+            pacman -Rns (pacman -Qtdq)
+        end
     end
-  end
 end
 
 # WSL
 
 if [ (uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ]
-  function cdw
-    cd /mnt/c/Users/shishi
-  end
+    function cdw
+        cd /mnt/c/Users/shishi
+    end
 end
 
 #function fci -d "Fuzzy-find and checkout a commit"
@@ -311,10 +312,10 @@ end
 # etc
 #########################################
 
-if type batcat &> /dev/null
-  ln -fs (which batcat) ~/.local/bin/bat
+if type batcat &>/dev/null
+    ln -fs (which batcat) ~/.local/bin/bat
 end
 
-if type fdfind &> /dev/null
-  ln -fs (which fdfind) ~/.local/bin/fd
+if type fdfind &>/dev/null
+    ln -fs (which fdfind) ~/.local/bin/fd
 end
