0fb851cfb5756925b8812d0709e0af46315ad873
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-13 16:29:19 +0900

    cmp tab mapping

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -66,7 +66,7 @@ vim.opt.smartcase = true
 vim.opt.wildmenu = true
 vim.opt.wildmode = 'list:longest,full'
 vim.opt.wildignorecase = true
-vim.opt.completeopt = 'menu,menuone,preview,noinsert,noselect'
+vim.opt.completeopt = 'menu,menuone,preview,noinsert'
 
 -- indent
 vim.opt.cindent = true
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -220,13 +220,13 @@ return packer.startup(function(use)
 				mapping = cmp.mapping.preset.insert({
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<Tab>'] = function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback()
-						end
-					end,
+					-- ['<Tab>'] = function(fallback)
+					-- 	if cmp.visible() then
+					-- 		cmp.select_next_item()
+					-- 	else
+					-- 		fallback()
+					-- 	end
+					-- end,
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
 					['<CR>'] = function(fallback)
