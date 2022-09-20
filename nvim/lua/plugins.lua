c6d862db58796438c98a818f58a5a991bb702200
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-20 21:32:13 +0900

    fzf

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1250,12 +1250,18 @@ return packer.startup(function(use)
         --   layout_config = {
         --     prompt_position = 'top',
         --   },
-        -- },
-        -- extensions = {
-        --   file_browser = {
-        --     hijack_netrw = true
-        --   }
-        -- }
+        extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          },
+          --   file_browser = {
+          --     hijack_netrw = true
+          --   }
+        },
       })
 
       -- builtin
