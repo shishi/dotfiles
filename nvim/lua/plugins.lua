cde104c7f33895c6d31945229454304511fb0f05
Shigenobu Nishikawa <shishi@srevo.net>
2022-10-15 00:57:25 +0900

    nvim-navic

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1306,29 +1306,29 @@ return packer.startup(function(use)
             },
           })
         end,
-        ['sqls'] = function()
-          lspconfig.sqls.setup({
-            capabilitiies = capabilities,
-            on_attach = function(client, bufnr)
-              navic.attach(client, bufnr)
-              require('sqls').on_attch(client, bufnr)
-            end,
-            settings = {
-              sqls = {
-                connections = {
-                  -- {
-                  --   driver = 'mysql',
-                  --   dataSourceName = 'root:root@tcp(127.0.0.1:3306)/world',
-                  -- },
-                  {
-                    driver = 'postgresql',
-                    dataSourceName = 'host=localhost port=5432 user=postgres password=password dbname=memie_dev sslmode=disable',
-                  },
-                },
-              },
-            },
-          })
-        end,
+        -- ['sqls'] = function()
+        --   lspconfig.sqls.setup({
+        --     capabilitiies = capabilities,
+        --     on_attach = function(client, bufnr)
+        --       navic.attach(client, bufnr)
+        --       require('sqls').on_attch(client, bufnr)
+        --     end,
+        --     settings = {
+        --       sqls = {
+        --         connections = {
+        --           -- {
+        --           --   driver = 'mysql',
+        --           --   dataSourceName = 'root:root@tcp(127.0.0.1:3306)/world',
+        --           -- },
+        --           {
+        --             driver = 'postgresql',
+        --             dataSourceName = 'host=localhost port=5432 user=postgres password=password dbname=memie_dev sslmode=disable',
+        --           },
+        --         },
+        --       },
+        --     },
+        --   })
+        -- end,
       })
     end,
   })
@@ -1337,6 +1337,39 @@ return packer.startup(function(use)
     'SmiteshP/nvim-navic',
     disable = vscode,
     requires = 'neovim/nvim-lspconfig',
+    config = function()
+      require('nvim-navic').setup({
+        -- highlight = true,
+      })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsFile', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsModule', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsNamespace', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsPackage', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsClass', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsMethod', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsProperty', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsField', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsConstructor', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsEnum', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsInterface', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsFunction', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsVariable', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsConstant', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsString', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsNumber', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsBoolean', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsArray', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsObject', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsKey', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsNull', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsEnumMember', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsStruct', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsEvent', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsOperator', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicIconsTypeParameter', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicText', { default = true, bg = '#000000', fg = '#ffffff' })
+      -- vim.api.nvim_set_hl(0, 'NavicSeparator', { default = true, bg = '#000000', fg = '#ffffff' })
+    end,
   })
 
   use({
