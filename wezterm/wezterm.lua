2abea023595313bdbe1ba4051feca302aaaebab8
Shigenobu Nishikawa <shishi@srevo.net>
2023-03-11 23:57:34 +0900

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,177 +1,187 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local act = wezterm.action
 
-wezterm.on("gui-startup", function(cmd)
+wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
   window:gui_window():set_position(850, 150)
 end)
 
 return {
-    front_end = "WebGpu",
-    default_prog = { "wsl.exe", "~", "-d", "Ubuntu", "--user", "shishi" },
+    front_end = 'WebGpu',
+    default_prog = { 'wsl.exe', '~', '-d', 'Ubuntu', '--user', 'shishi' },
     -- default_prog = { "cmd.exe", "/c", "wsl.exe ~ -d Ubuntu" },
     launch_menu = {
         {
-            label = "Command Prompt",
-            args = { "cmd.exe" },
+            label = 'Command Prompt',
+            args = { 'cmd.exe' },
         },
         {
-            label = "Powershell",
-            args = { "C:/Program Files/PowerShell/7/pwsh.exe" },
+            label = 'Powershell',
+            args = { 'C:/Program Files/PowerShell/7/pwsh.exe' },
         },
     },
     -- seems this setting effect only QuitApplication
-    window_close_confirmation = "NeverPrompt",
+    window_close_confirmation = 'NeverPrompt',
     skip_close_confirmation_for_processes_named = {
-        "bash",
-        "sh",
-        "zsh",
-        "fish",
-        "tmux",
-        "nu",
-        "cmd.exe",
-        "pwsh.exe",
-        "powershell.exe",
-        "wsl.exe",
-        "wslhost.exe",
-        "conhost.exe",
+        'bash',
+        'sh',
+        'zsh',
+        'fish',
+        'tmux',
+        'nu',
+        'cmd.exe',
+        'pwsh.exe',
+        'powershell.exe',
+        'wsl.exe',
+        'wslhost.exe',
+        'conhost.exe',
     },
     -- key
     disable_default_key_bindings = true,
     keys = {
         -- applicaton
         {
-            key = "q",
-            mods = "CTRL|SHIFT",
+            key = 'q',
+            mods = 'CTRL|SHIFT',
             action = act.QuitApplication,
         },
         {
-            key = "l",
-            mods = "CTRL|SHIFT",
+            key = 'l',
+            mods = 'CTRL|SHIFT',
             action = act.ShowLauncher,
         },
         {
-            key = "Backspace",
-            mods = "CTRL|SHIFT",
+            key = 'Backspace',
+            mods = 'CTRL|SHIFT',
             action = act.ActivateCopyMode,
         },
         -- tab & pane
         {
-            key = "t",
-            mods = "CTRL|SHIFT",
+            key = 't',
+            mods = 'CTRL|SHIFT',
             action = act.SpawnCommandInNewTab,
         },
         {
-            key = "w",
-            mods = "CTRL|SHIFT",
+            key = 'w',
+            mods = 'CTRL|SHIFT',
             action = act.CloseCurrentPane({
                 confirm = false,
             }),
         },
         {
             key = "'",
-            mods = "CTRL|SHIFT|ALT",
+            mods = 'CTRL|SHIFT|ALT',
             action = wezterm.action.SplitVertical({
-                domain = "CurrentPaneDomain",
+                domain = 'CurrentPaneDomain',
             }),
         },
         {
-            key = ";",
-            mods = "CTRL|SHIFT|ALT",
+            key = ';',
+            mods = 'CTRL|SHIFT|ALT',
             action = wezterm.action.SplitHorizontal({
-                domain = "CurrentPaneDomain",
+                domain = 'CurrentPaneDomain',
             }),
         },
         {
-            key = "UpArrow",
-            mods = "SHIFT|CTRL",
-            action = act.ActivatePaneDirection("Up"),
+            key = 'UpArrow',
+            mods = 'SHIFT|CTRL',
+            action = act.ActivatePaneDirection('Up'),
         },
         {
-            key = "DownArrow",
-            mods = "SHIFT|CTRL",
-            action = act.ActivatePaneDirection("Down"),
+            key = 'DownArrow',
+            mods = 'SHIFT|CTRL',
+            action = act.ActivatePaneDirection('Down'),
         },
         {
-            key = "RightArrow",
-            mods = "SHIFT|CTRL",
-            action = act.ActivatePaneDirection("Right"),
+            key = 'RightArrow',
+            mods = 'SHIFT|CTRL',
+            action = act.ActivatePaneDirection('Right'),
         },
         {
-            key = "LeftArrow",
-            mods = "SHIFT|CTRL",
-            action = act.ActivatePaneDirection("Left"),
+            key = 'LeftArrow',
+            mods = 'SHIFT|CTRL',
+            action = act.ActivatePaneDirection('Left'),
         },
         {
-            key = "1",
-            mods = "SHIFT|CTRL",
+            key = 'Tab',
+            mods = 'CTRL',
+            action = act.ActivateTabRelative(1),
+        },
+        {
+            key = 'Tab',
+            mods = 'SHIFT|CTRL',
+            action = act.ActivateTabRelative( -1),
+        },
+        {
+            key = '1',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(0),
         },
         {
-            key = "2",
-            mods = "SHIFT|CTRL",
+            key = '2',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(1),
         },
         {
-            key = "3",
-            mods = "SHIFT|CTRL",
+            key = '3',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(2),
         },
         {
-            key = "4",
-            mods = "SHIFT|CTRL",
+            key = '4',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(3),
         },
         {
-            key = "5",
-            mods = "SHIFT|CTRL",
+            key = '5',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(4),
         },
         {
-            key = "6",
-            mods = "SHIFT|CTRL",
+            key = '6',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(5),
         },
         {
-            key = "7",
-            mods = "SHIFT|CTRL",
+            key = '7',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(6),
         },
         {
-            key = "8",
-            mods = "SHIFT|CTRL",
+            key = '8',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab(7),
         },
         {
-            key = "9",
-            mods = "SHIFT|CTRL",
+            key = '9',
+            mods = 'SHIFT|CTRL',
             action = act.ActivateTab( -1),
         },
         -- copy & paste
         {
-            key = "c",
-            mods = "CTRL|SHIFT",
-            action = act.CopyTo("Clipboard"),
+            key = 'c',
+            mods = 'CTRL|SHIFT',
+            action = act.CopyTo('Clipboard'),
         },
         {
-            key = "v",
-            mods = "CTRL|SHIFT",
-            action = act.PasteFrom("Clipboard"),
+            key = 'v',
+            mods = 'CTRL|SHIFT',
+            action = act.PasteFrom('Clipboard'),
         },
         -- font size
         {
-            key = "0",
-            mods = "CTRL",
+            key = '0',
+            mods = 'CTRL',
             action = act.ResetFontSize,
         },
         {
-            key = "-",
-            mods = "CTRL|SHIFT",
+            key = '-',
+            mods = 'CTRL|SHIFT',
             action = act.DecreaseFontSize,
         },
         {
-            key = "=",
-            mods = "CTRL|SHIFT",
+            key = '=',
+            mods = 'CTRL|SHIFT',
             action = act.IncreaseFontSize,
         },
     },
@@ -181,36 +191,36 @@ return {
             event = {
                 Up = {
                     streak = 1,
-                    button = "Middle",
+                    button = 'Middle',
                 },
             },
-            mods = "NONE",
+            mods = 'NONE',
             action = act.Paste,
         },
         {
             event = {
                 Up = {
                     streak = 1,
-                    button = "Right",
+                    button = 'Right',
                 },
             },
-            mods = "NONE",
+            mods = 'NONE',
             action = act.Copy,
         },
     },
     -- GUI
     font = wezterm.font_with_fallback({
         {
-            family = "UDEV Gothic NF",
+            family = 'UDEV Gothic NF',
         },
         {
-            family = "Noto Color Emoji",
+            family = 'Noto Color Emoji',
         },
     }),
     font_size = 20,
     adjust_window_size_when_changing_font_size = false,
     -- color_scheme = "Gruvbox dark, hard (base16)",
-    color_scheme = "Afterglow",
+    color_scheme = 'Afterglow',
     window_background_opacity = 0.9,
     initial_cols = 160,
     initial_rows = 42,
@@ -229,8 +239,8 @@ return {
         -- main font setting appended to it to pick up any
         -- fallback fonts you may have used there.
         font = wezterm.font({
-            family = "Roboto",
-            weight = "Bold",
+            family = 'Roboto',
+            weight = 'Bold',
         }),
 
         -- The size of the font in the tab bar.
@@ -239,16 +249,16 @@ return {
 
         -- The overall background color of the tab bar when
         -- the window is focused
-        active_titlebar_bg = "#333333",
+        active_titlebar_bg = '#333333',
 
         -- The overall background color of the tab bar when
         -- the window is not focused
-        inactive_titlebar_bg = "#333333",
+        inactive_titlebar_bg = '#333333',
     },
     colors = {
         tab_bar = {
             -- The color of the inactive tab bar edge/divider
-            inactive_tab_edge = "#575757",
+            inactive_tab_edge = '#575757',
         },
     },
 }
