0b6f135a0a2cc102b13cea029055f1e87a7cd09c
Shigenobu Nishikawa <shishi@srevo.net>
2023-07-24 10:53:49 +0900

    confict leap and surroud

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -625,7 +625,20 @@ local plugins = {
       })
 
       require('mini.pairs').setup()
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = '<Leader>sa', -- Add surrounding in Normal and Visual modes
+          delete = '<Leader>sd', -- Delete surrounding
+          find = '<Leader>sf', -- Find surrounding (to the right)
+          find_left = '<Leader>sF', -- Find surrounding (to the left)
+          highlight = '<Leader>sh', -- Highlight surrounding
+          replace = '<Leader>sr', -- Replace surrounding
+          update_n_lines = '<Leader>sn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      })
     end,
   },
   {
