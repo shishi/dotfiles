de3c9ad1d476084e201e47c408f9db7ef2d81435
Shigenobu Nishikawa <shishi@srevo.net>
2023-08-16 01:55:36 +0900

    use callback

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -2,11 +2,10 @@
 vim.api.nvim_create_augroup('ft_golang', { clear = true })
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   group = 'ft_golang',
-  pattern = 'go',
-  -- callback = function()
-  --   vim.opt_local.expandtab = false
-  --   vim.opt_local.noexpandtab = true
-  --   vim.opt_local.tabstop = 4
-  -- end,
-  command = 'setlocal noexpandtab tabstop=4',
+  pattern = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    -- vim.cmd('setlocal noexpandtab tabstop=4')
+  end,
 })
