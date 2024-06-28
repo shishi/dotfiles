3d402cf7f8db483f87c89e5efe6754a56fdfc135
Shigenobu Nishikawa <shishi@srevo.net>
2024-06-28 15:33:43 +0900

    trouble

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1814,10 +1814,13 @@ local plugins = {
       { 'nvim-tree/nvim-web-devicons' },
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-treesitter/nvim-treesitter' },
+      { 'folke/trouble.nvim' },
     },
     config = function()
-      local trouble = require('trouble.providers.telescope')
+      local open_with_trouble = require('trouble.sources.telescope').open
+      -- local add_to_trouble = require('trouble.sources.telescope').add
       local lga_actions = require('telescope-live-grep-args.actions')
+
       require('telescope').setup({
         defaults = {
           path_display = {
@@ -1830,10 +1833,10 @@ local plugins = {
           -- },
           mappings = {
             i = {
-              ['<c-t>'] = trouble.open_with_trouble,
+              ['<c-t>'] = open_with_trouble,
             },
             n = {
-              ['<c-t>'] = trouble.open_with_trouble,
+              ['<c-t>'] = open_with_trouble,
             },
           },
         },
@@ -2036,24 +2039,43 @@ local plugins = {
   },
   {
     'folke/trouble.nvim',
-    cond = not_in_vscode,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-    config = function()
-      require('trouble').setup({
-        icons = true,
-      })
-
-      vim.keymap.set('n', '<Leader>xx', '<Cmd>TroubleToggle<CR>', {})
-      vim.keymap.set('n', '<Leader>xw', '<Cmd>TroubleToggle workspace_diagnostics<CR>', {})
-      vim.keymap.set('n', '<Leader>xd', '<Cmd>TroubleToggle document_diagnostics<CR>', {})
-      vim.keymap.set('n', '<Leader>xl', '<Cmd>TroubleToggle loclist<CR>', {})
-      vim.keymap.set('n', '<Leader>xq', '<Cmd>TroubleToggle quickfix<CR>', {})
-      -- vim.keymap.set('n', 'gR', '<Cmd>TroubleToggle lsp_references<CR>', {})
-      --
-      vim.cmd([[
-        cnoreabbrev copen TroubleToggle quickfix
-      ]])
-    end,
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
   },
   {
     'mbbill/undotree',
