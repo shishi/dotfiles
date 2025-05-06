4a68dcb9f1b3f97b9790fbb7460d63a5e638576d
Shigenobu Nishikawa <shishi@srevo.net>
2025-05-06 17:56:05 +0900

    lspkind

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -779,8 +779,12 @@ later(function()
       'rafamadriz/friendly-snippets',
     },
   })
+  add({
+    source = 'onsails/lspkind.nvim',
+  })
 
   local cmp = require('cmp')
+  local lspkind = require('lspkind')
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
@@ -888,6 +892,27 @@ later(function()
       { name = 'buffer' },
       { name = 'path' },
     }),
+    formatting = {
+      format = lspkind.cmp_format({
+        mode = 'symbol_text', -- show only symbol annotations
+        maxwidth = {
+          -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          -- can also be a function to dynamically calculate max width such as
+          -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+          menu = 50, -- leading text (labelDetails)
+          abbr = 50, -- actual suggestion item
+        },
+        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+        -- The function below will be called before any actual modifications from lspkind
+        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+        before = function(entry, vim_item)
+          -- ...
+          return vim_item
+        end,
+      }),
+    },
   })
   -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
   -- Set configuration for specific filetype.
