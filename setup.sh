c91f83bd44e3472d3deaffb6352ef7fc7eecee53
Shigenobu Nishikawa <shishi@srevo.net>
2025-06-20 15:37:00 +0900

    claude code

--- setup.sh
+++ setup.sh
@@ -1,83 +1,93 @@
 #!/bin/bash
 
 if [ -d /.jbdevcontainer ]; then
-    XDG_CONFIG_HOME=/.jbdevcontainer/config
+  XDG_CONFIG_HOME=/.jbdevcontainer/config
 elif [ -z $XDG_CONFIG_HOME ]; then
-    XDG_CONFIG_HOME=$HOME/.config
-    mkdir -p ~/.config
+  XDG_CONFIG_HOME=$HOME/.config
+  mkdir -p ~/.config
 fi
 
 DOTDIR=$(realpath $(dirname "$0"))
 #$EMACSDIR=~/dev/src/github.com/shishi/emacs
 
 if [ "$REMOTE_CONTAINERS" != true ]; then
-    if [ -L ${XDG_CONFIG_HOME}/wezterm ]; then
-        rm ${XDG_CONFIG_HOME}/wezterm
-        ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
-    elif [ -d ${XDG_CONFIG_HOME}/wezterm ]; then
-        rm -fr ${XDG_CONFIG_HOME}/wezterm
-        ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
-    else
-        ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
-    fi
+  if [ -L ${XDG_CONFIG_HOME}/wezterm ]; then
+    rm ${XDG_CONFIG_HOME}/wezterm
+    ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
+  elif [ -d ${XDG_CONFIG_HOME}/wezterm ]; then
+    rm -fr ${XDG_CONFIG_HOME}/wezterm
+    ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
+  else
+    ln -sf ${DOTDIR}/wezterm ${XDG_CONFIG_HOME}/wezterm
+  fi
 
-    if [ -L ~/.emacs.d ]; then
-        rm ~/.emacs.d
-        ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
-    elif [ -d ~/.emacs.d ]; then
-        git -C $(dirname ${DOTDIR}) clone git@github.com:shishi/emacs.git
-        rm -fr ~/.emacs.d
-        ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
-    else
-        git -C $(dirname ${DOTDIR}) clone git@github.com:shishi/emacs.git
-        ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
-    fi
+  if [ -L ~/.emacs.d ]; then
+    rm ~/.emacs.d
+    ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
+  elif [ -d ~/.emacs.d ]; then
+    git -C $(dirname ${DOTDIR}) clone git@github.com:shishi/emacs.git
+    rm -fr ~/.emacs.d
+    ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
+  else
+    git -C $(dirname ${DOTDIR}) clone git@github.com:shishi/emacs.git
+    ln -sf $(dirname ${DOTDIR})/emacs ~/.emacs.d
+  fi
 fi
 
 if [ -L ${XDG_CONFIG_HOME}/fish ]; then
-    rm ${XDG_CONFIG_HOME}/fish
-    ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
+  rm ${XDG_CONFIG_HOME}/fish
+  ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
 elif [ -d ${XDG_CONFIG_HOME}/fish ]; then
-    rm -fr ${XDG_CONFIG_HOME}/fish
-    ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
+  rm -fr ${XDG_CONFIG_HOME}/fish
+  ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
 else
-    ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
+  ln -sf ${DOTDIR}/fish ${XDG_CONFIG_HOME}/fish
 fi
 
 if [ -L ${XDG_CONFIG_HOME}/nvim ]; then
-    rm ${XDG_CONFIG_HOME}/nvim
-    ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
+  rm ${XDG_CONFIG_HOME}/nvim
+  ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
 elif [ -d ${XDG_CONFIG_HOME}/nvim ]; then
-    rm -fr ${XDG_CONFIG_HOME}/nvim
-    ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
+  rm -fr ${XDG_CONFIG_HOME}/nvim
+  ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
 else
-    ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
+  ln -sf ${DOTDIR}/nvim ${XDG_CONFIG_HOME}/nvim
 fi
 
 if [ -L ${XDG_CONFIG_HOME}/helix ]; then
-    rm ${XDG_CONFIG_HOME}/helix
-    ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
+  rm ${XDG_CONFIG_HOME}/helix
+  ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
 elif [ -d ${XDG_CONFIG_HOME}/helix ]; then
-    rm -fr ${XDG_CONFIG_HOME}/helix
-    ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
+  rm -fr ${XDG_CONFIG_HOME}/helix
+  ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
 else
-    ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
+  ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
 fi
 
-if [ `uname` = Darwin ]; then
-    ln -sf ${DOTDIR}/.gitconfig.mac ~/.gitconfig
-    ln -sf ${DOTDIR}/Brewfile ~/Brewfile
-elif [ `uname` = Linux ]; then
-    ln -sf ${DOTDIR}/.gitconfig.linux ~/.gitconfig
-    #    ln -sf ${DOTDIR}/terminator ${XDG_CONFIG_HOME}/terminator
-    #    ln -sf ${DOTDIR}/.xprofile ~/.xprofile
-    #    ln -sf ${DOTDIR}/.xbindkeysrc ~/.xbindkeysrc
-    #    ln -sf ${DOTDIR}/.imwheelrc ~/.imwheelrc
-    #    ln -sf ${DOTDIR}/imwheel.desktop ${XDG_CONFIG_HOME}/autostart/imwheel.desktop
-    #    ln -sf ${DOTDIR}/fonts.conf ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
-    #    fc-cache -fv
-elif [ `uname` = MINGW64_NT-10.0 ]; then
-    ln -sf ${DOTDIR}/.gitconfig.win ~/.gitconfig
+if [ -L ${XDG_CONFIG_HOME}/claude ]; then
+  rm ${XDG_CONFIG_HOME}/claude
+  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+elif [ -d ${XDG_CONFIG_HOME}/claude ]; then
+  rm -fr ${XDG_CONFIG_HOME}/claude
+  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+else
+  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+fi
+
+if [ $(uname) = Darwin ]; then
+  ln -sf ${DOTDIR}/.gitconfig.mac ~/.gitconfig
+  ln -sf ${DOTDIR}/Brewfile ~/Brewfile
+elif [ $(uname) = Linux ]; then
+  ln -sf ${DOTDIR}/.gitconfig.linux ~/.gitconfig
+  #    ln -sf ${DOTDIR}/terminator ${XDG_CONFIG_HOME}/terminator
+  #    ln -sf ${DOTDIR}/.xprofile ~/.xprofile
+  #    ln -sf ${DOTDIR}/.xbindkeysrc ~/.xbindkeysrc
+  #    ln -sf ${DOTDIR}/.imwheelrc ~/.imwheelrc
+  #    ln -sf ${DOTDIR}/imwheel.desktop ${XDG_CONFIG_HOME}/autostart/imwheel.desktop
+  #    ln -sf ${DOTDIR}/fonts.conf ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
+  #    fc-cache -fv
+elif [ $(uname) = MINGW64_NT-10.0 ]; then
+  ln -sf ${DOTDIR}/.gitconfig.win ~/.gitconfig
 fi
 
 ln -sf ${DOTDIR}/.gitignore.global ~/.gitignore
