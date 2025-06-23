300964e98d871992c6ec1097a7c49a238c89ac43
Shigenobu Nishikawa <shishi@srevo.net>
2025-06-23 10:48:57 +0900

    claude

--- setup.sh
+++ setup.sh
@@ -64,15 +64,17 @@ else
   ln -sf ${DOTDIR}/helix ${XDG_CONFIG_HOME}/helix
 fi
 
-if [ -L ${XDG_CONFIG_HOME}/claude ]; then
-  rm ${XDG_CONFIG_HOME}/claude
-  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
-elif [ -d ${XDG_CONFIG_HOME}/claude ]; then
-  rm -fr ${XDG_CONFIG_HOME}/claude
-  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
-else
-  ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
-fi
+ln -sf ${DOTDIR}/claude/settings.json ~/.claude/settings.json
+ln -sf ${DOTDIR}/claude/CLAUDE.md ~/.claude/CLAUDE.md
+# if [ -L ${XDG_CONFIG_HOME}/claude ]; then
+#   rm ${XDG_CONFIG_HOME}/claude
+#   ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+# elif [ -d ${XDG_CONFIG_HOME}/claude ]; then
+#   rm -fr ${XDG_CONFIG_HOME}/claude
+#   ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+# else
+#   ln -sf ${DOTDIR}/claude ${XDG_CONFIG_HOME}/claude
+# fi
 
 if [ $(uname) = Darwin ]; then
   ln -sf ${DOTDIR}/.gitconfig.mac ~/.gitconfig
