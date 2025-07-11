1ffad5e5e660193db3ff01778e1bd475128dd229
Shigenobu Nishikawa <shishi@srevo.net>
2025-07-11 23:06:57 +0900

    remove comment

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -975,14 +975,6 @@ later(function()
     }),
     matching = { disallow_symbol_nonprefix_matching = false },
   })
-
-  -- moved to lsp/init.lua
-  -- -- Set up lspconfig.
-  -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup({
-  --   capabilities = capabilities,
-  -- })
 end)
 
 -- later(function()
