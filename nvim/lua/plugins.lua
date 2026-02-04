49469b07d9a0f4e0a9b1f669d2cdc07c8361db10
Shigenobu Nishikawa <shishi@srevo.net>
2026-02-04 22:33:56 +0900

    extraKnownMarketplaces

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -975,7 +975,7 @@ later(function()
   end, {
     desc = 'telescope current_buffer_fuzzy_find',
   })
-  vim.keymap.set('n', '<C-P>', function()
+  vim.keymap.set('n', '<C-q>', function()
     require('telescope.builtin').buffers()
   end, {
     desc = 'telescope buffers',
