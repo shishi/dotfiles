af337b8f9bb7091654f6a3d964d191bf9c80be26
Shigenobu Nishikawa <shishi@srevo.net>
2026-08-13 20:18:29 +0900

    typo

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -62,7 +62,7 @@ vim.opt.wildignorecase = true
 vim.opt.completeopt = 'menu,menuone,preview,noinsert'
 
 if vim.fn.executable('rg') == 1 then
-  vim.opt.grepprg = 'rg --vimgrep --color=never --smart-case --uu --glob "!.git/"'
+  vim.opt.grepprg = 'rg --vimgrep --color=never --smart-case -uu --glob "!.git/"'
 end
 
 -- indent
